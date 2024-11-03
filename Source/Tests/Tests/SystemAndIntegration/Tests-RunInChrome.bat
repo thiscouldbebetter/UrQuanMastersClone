017752ed607c10dd29fcc5307961707cd3514f6f
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2024-11-03 11:34:39 -0500

    Added StartDevEnvironment.bat.

--- Source/Tests/Tests/SystemAndIntegration/Tests-RunInChrome.bat
+++ Source/Tests/Tests/SystemAndIntegration/Tests-RunInChrome.bat
@@ -3,4 +3,4 @@ rem 1. Chrome is not installed,
 rem 2. Chrome's path is not part of the "PATH" environment variable, or
 rem 3. any Chrome window is already running without the "--allow-file-access-from-files" switch.
 
-chrome --allow-file-access-from-files %cd%/Tests.html
+start chrome --allow-file-access-from-files %cd%/Tests.html
---
+++ Source/UrQuanMastersClone-StartDevEnvironment.bat
@@ -0,0 +1,9 @@
+start explorer .
+start notepad++ UrQuanMastersClone.html
+start notepad++ Tests/Tests/SystemAndIntegration/SystemTests.ts
+call UrQuanMastersClone-RunInChrome.bat
+cd Tests/Tests/SystemAndIntegration/
+call Tests-RunInChrome.bat
+cd ../../..
+cd ../Content
+start explorer .
